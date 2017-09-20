a6ec8eed026a0a86dd38fbc6725aabfcaed3c9e6
Christopher Osborn <cosborn@cimpress.com>
2017-09-20 18:46:57 -0400

    I Found the Setting for Documentation Headers
    [yay]

--- src/TypeTransformer.cs
+++ src/TypeTransformer.cs
@@ -1,3 +1,19 @@
+// <copyright file="TypeTransformer.cs" company="Cimpress, Inc.">
+//   Copyright 2017 Cimpress, Inc.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +27,7 @@ namespace Tiger.Hal
     sealed class TypeTransformer
         : ITypeTransformer
     {
-        static readonly EqualityComparer _comparer = new EqualityComparer(Ordinal);
+        static readonly EqualityComparer s_comparer = new EqualityComparer(Ordinal);
 
         readonly ITransformationInstructions _transformationInstructions;
         readonly IUrlHelper _urlHelper;
@@ -49,7 +65,7 @@ namespace Tiger.Hal
                 .SelectMany(
                     kvp => kvp.Value.TransformToLinkBuilders(value),
                     (kvp, lb) => (rel: kvp.Key, isSingular: kvp.Value.IsSingular, link: lb.Build(_urlHelper)))
-                .ToLookup(kvp => (kvp.rel, kvp.isSingular), kvp => kvp.link, _comparer)
+                .ToLookup(kvp => (kvp.rel, kvp.isSingular), kvp => kvp.link, s_comparer)
                 .ToDictionary(g => g.Key.rel, g => new LinkCollection(g.ToList(), g.Key.isSingular));
         }
 
